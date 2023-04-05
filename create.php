0f83c65c6588abbb708b771f64785d46dd0f557d
Pablo Guevara <pablo_alexa22@hotmail.com>
2023-04-05 11:31:20 -0500

    Segunda Version

--- create.php
+++ create.php
@@ -7,11 +7,13 @@
 	if(isset($_POST['submit'])) {
 		// Recuperar los datos del formulario
 		$nombre = $_POST['nombre'];
+		$apellido = $_POST['apellido'];
 		$correo = $_POST['correo'];
-		$tipo = $_POST['tipo_usuario'];
+		$tipo_usuario = $_POST['tipo_usuario'];
+		$contrasena = $_POST['contrasena'];
 
 		// Insertar los datos en la base de datos
-		$query = "INSERT INTO usuarios(nombre, correo, tipo_usuario) VALUES('$nombre', '$correo', '$tipo')";
+		$query = "INSERT INTO usuarios(nombre,apellido, correo, tipo_usuario, contrasena) VALUES('$nombre', '$apellido','$correo', '$tipo_usuario','$contrasena')";
 		$resultado = mysqli_query($conn, $query);
 
 		// Redirigir al usuario de regreso a la página principal
@@ -34,16 +36,20 @@
 		<label for="nombre">Nombre:</label>
 		<input type="text" id="nombre" name="nombre" required>
 
+		<label for="apellido">Apellido:</label>
+		<input type="text" id="apellido" name="apellido" required>
+
 		<label for="correo">Correo electrónico:</label>
 		<input type="email" id="correo" name="correo" required>
 
 		<label for="tipo">Tipo de usuario:</label>
 		<select id="tipo" name="tipo" required>
 			<option value="">Seleccione el tipo de usuario</option>
-			<option value="j">Jurado</option>
-			<option value="p">Participante</option>
+			<option value="jurado">Jurado</option>
+			<option value="participante">Participante</option>
 		</select>
-
+		<label for="contrasena">Contraseña temporal:</label>
+		<input type="password" id="contrasena" name="contrasena" required>
 		<input type="submit" name="submit" value="Agregar">
 	</form>
 

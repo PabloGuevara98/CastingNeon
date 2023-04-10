d1dd7c2421983c3b66483c534d78fe42a37a838d
Pablo Guevara <pablo_alexa22@hotmail.com>
2023-04-10 09:00:42 -0500

    Agregar validacion de tipos de usuarios

--- inicio_jurado.php
+++ inicio_jurado.php
@@ -1,10 +1,45 @@
 <?php
 session_start();
+include('config.php');
 
-if (!isset($_SESSION['id'])) {
-  header("location: index.php");
+// Verificar si el usuario está logueado como jurado
+if ($_SESSION['tipo_usuario'] != 'jurado') {
+  header('Location: index.php');
 }
 
-echo "¡Bienvenido, jurado " . $_SESSION['nombre_usuario'] . "!<br>";
-echo "<a href='logout.php'>Cerrar sesión</a>";
+// Consulta para traer los datos de los castings y los participantes inscritos en cada cast
+$sql = "SELECT c.*, GROUP_CONCAT(u.nombre SEPARATOR ', ') AS participantes FROM casts c
+        LEFT JOIN inscripciones i ON i.id_cast = c.id
+        LEFT JOIN usuarios u ON u.id = i.id_usuario
+        GROUP BY c.id";
+
+$resultado = mysqli_query($conexion, $sql);
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Inicio Jurado</title>
+</head>
+<body>
+  <h1>Bienvenido, <?php echo $_SESSION['nombre_usuario']; ?></h1>
+
+  <h2>Lista de Castings</h2>
+  <ul>
+    <?php while ($fila = mysqli_fetch_array($resultado)) { ?>
+      <li>
+        <h3><?php echo $fila['nombre']; ?></h3>
+        <p><strong>Fecha:</strong> <?php echo $fila['fecha']; ?></p>
+        <p><strong>Hora:</strong> <?php echo $fila['hora']; ?></p>
+        <p><strong>Participantes:</strong> <?php echo $fila['participantes']; ?></p>
+        <form method="post" action="inscribirse_cast.php">
+          <input type="hidden" name="id_cast" value="<?php echo $fila['id']; ?>">
+          <button type="submit">Inscribirse</button>
+        </form>
+      </li>
+    <?php } ?>
+  </ul>
+
+</body>
+</html>

21dff91ef4a4b556cb84bb87b62de2bdf9ac9e95
Pablo Guevara <pablo_alexa22@hotmail.com>
2023-04-10 09:50:35 -0500

    Funcionalidad Ver Participantes  Inscritos

--- inicio_jurado.php
+++ inicio_jurado.php
@@ -4,42 +4,41 @@ include('config.php');
 
 // Verificar si el usuario está logueado como jurado
 if ($_SESSION['tipo_usuario'] != 'jurado') {
-  header('Location: index.php');
+  header('Location: inicio_jurado.php');
 }
-
-// Consulta para traer los datos de los castings y los participantes inscritos en cada cast
-$sql = "SELECT c.*, GROUP_CONCAT(u.nombre SEPARATOR ', ') AS participantes FROM casts c
-        LEFT JOIN inscripciones i ON i.id_cast = c.id
-        LEFT JOIN usuarios u ON u.id = i.id_usuario
-        GROUP BY c.id";
+// Realizar la consulta a la base de datos
+$sql = "SELECT casts.id_cast, casts.fecha_cast, casts.hora_cast, casts.nombre_cast, 
+               COUNT(inscripciones.id_cast) as num_participantes 
+        FROM casts 
+        LEFT JOIN inscripciones ON casts.id_cast = inscripciones.id_cast 
+        GROUP BY casts.id_cast";
 
 $resultado = mysqli_query($conexion, $sql);
 
+// Mostrar los resultados en una tabla
+echo "<table>";
+echo "<tr><th>ID</th><th>Fecha</th><th>Hora</th><th>Nombre</th><th>Número de participantes</th><th>Participantes inscritos</th></tr>";
+while ($fila = mysqli_fetch_array($resultado)) {
+    echo "<tr>";
+    echo "<td>" . $fila['id'] . "</td>";
+    echo "<td>" . $fila['fecha'] . "</td>";
+    echo "<td>" . $fila['hora'] . "</td>";
+    echo "<td>" . $fila['nombre'] . "</td>";
+    echo "<td>" . $fila['num_participantes'] . "</td>";
+
+    // Mostrar los participantes inscritos en este cast
+    echo "<td>";
+    $sql_participantes = "SELECT usuarios.nombre FROM usuarios 
+                          INNER JOIN inscripciones ON usuarios.id = inscripciones.usuario_id 
+                          WHERE inscripciones.cast_id = " . $fila['id'];
+
+    $resultado_participantes = mysqli_query($conexion, $sql_participantes);
+    while ($fila_participantes = mysqli_fetch_array($resultado_participantes)) {
+        echo $fila_participantes['nombre'] . "<br>";
+    }
+    echo "</td>";
+
+    echo "</tr>";
+}
+echo "</table>";
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Inicio Jurado</title>
-</head>
-<body>
-  <h1>Bienvenido, <?php echo $_SESSION['nombre_usuario']; ?></h1>
-
-  <h2>Lista de Castings</h2>
-  <ul>
-    <?php while ($fila = mysqli_fetch_array($resultado)) { ?>
-      <li>
-        <h3><?php echo $fila['nombre']; ?></h3>
-        <p><strong>Fecha:</strong> <?php echo $fila['fecha']; ?></p>
-        <p><strong>Hora:</strong> <?php echo $fila['hora']; ?></p>
-        <p><strong>Participantes:</strong> <?php echo $fila['participantes']; ?></p>
-        <form method="post" action="inscribirse_cast.php">
-          <input type="hidden" name="id_cast" value="<?php echo $fila['id']; ?>">
-          <button type="submit">Inscribirse</button>
-        </form>
-      </li>
-    <?php } ?>
-  </ul>
-
-</body>
-</html>
